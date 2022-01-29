561a3b9e4283eed25b9605f4d9b559340e6d8d76
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-01-29 11:23:33 +0530

    Updating the approaches

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -2,10 +2,15 @@
 	Binary Search
 	Approach 01 : Recursive approach
 	Approach 02 : Iterative approach
-	Approach 03 : Any Improvements possible?
+
+	@Note: It is general advice for all the coder, prefer iterative binary search instead recursion.
+	but writing the recursion code sometime not easy and intutive so as per comfirtable level write your code
+	but incase your code is recursive, always careful about the recursive stack.
+
 	@copyright : Abinash Mishra, You can use for understanding the concept.
 */
-
+#define FORREF
+#ifndef FORREF
 using namespace std;
 #include<iostream>
 
@@ -21,7 +26,7 @@ struct BinarySearch
 			}
 			return -1;
 		}
-		
+
 		int mid = left + (right - left) / 2;
 		int res = -1;
 		if (arr[mid] == target)
@@ -35,15 +40,36 @@ struct BinarySearch
 		else
 		{
 			// check the left half
-			res = BinarySearch_rec(arr, left, mid-1, target);
+			res = BinarySearch_rec(arr, left, mid - 1, target);
 		}
 
 		return res;
 	}
-
-	int BinarySearch_iter(int arr[], int left, int right) 
+	int BinarySearch_iter(int arr[], int left, int right, int target)
 	{
+		// there is no base condition, you have to iterate and start grow until the point you are looking
+		while (left <= right) {
+			if (left == right)
+			{
+				if (arr[left] == target)
+				{
+					return left;
+				}
+				return -1;
+			}
 
+			int mid = left + (right - left) / 2;
+			if (arr[mid] == target)
+			{
+				return mid;
+			}
+			else if (arr[mid] < target)
+			{
+				left = mid + 1;
+			}
+			else
+				right = mid - 1;
+		}
 
 		return -1;
 	}
@@ -51,11 +77,11 @@ struct BinarySearch
 
 int main()
 {
-	int arr[] = { 2, 3, 4, 10, 40 };
-	int x = 10;
+	int arr[] = { 2, 3, 4, 10, 40, 41 };
+	int x = 32;
 	int n = sizeof(arr) / sizeof(arr[0]);
 	BinarySearch bs;
-	int result = bs.BinarySearch_rec(arr, 0, n - 1, x);
+	int result = bs.BinarySearch_iter(arr, 0, n - 1, x);
 	if (result == -1)
 		cout << "Element is not present in array";
 	else
@@ -63,3 +89,5 @@ int main()
 
 	return 0;
 }
+#endif // FORREF
+
