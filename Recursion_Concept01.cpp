b54f12f8c439aba2b0d20218f7e36f72f86660b8
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-01-30 19:55:29 +0530

    Recursion concept started...

---
+++ Recursion_Concept01.cpp
@@ -0,0 +1,47 @@
+/*
+	Multiplication Without Using * Operator
+	Meaning you want to implement a*b without using operaot '*'
+
+	Problem statement
+	In this problem, we need to find the result of the 
+	multiplication of two numbers, a and b but without using * operator.
+
+	one catch here is you have to implement this using recursion only.
+	Implementation:
+*/
+
+#define FORREF
+#ifndef FORREF
+using namespace std;
+#include<iostream>
+
+
+struct Multiplication
+{
+	int recAddition(int a, int b) {
+		if (b == 0) {
+			return 0;
+		}
+		else if (b == 1) {
+			return a;
+		}
+		else if (b < 0) {
+			b = -b;
+			--b;
+			return (-a + recAddition(a, -b));
+		}
+		else {
+			return (a + recAddition(a, b - 1));
+		}
+	}
+};
+
+int main() {
+	int a = 0, b = 5;
+	Multiplication mul;
+	cout << "answer = " << mul.recAddition(a, b) << endl;
+
+}
+#endif // !FORREF
+
+
