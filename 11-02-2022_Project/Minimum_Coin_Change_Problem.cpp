1a5dc9fc994d4b2a97162ad84abaf78b9fc06331
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-02-13 01:39:41 +0530

    Bruteforce Approach

--- 11-02-2022_Project/Minimum_Coin_Change_Problem.cpp
+++ 11-02-2022_Project/Minimum_Coin_Change_Problem.cpp
@@ -13,20 +13,29 @@ typedef vector<int> Memo;
 
 struct CoinChange {
 	/*
+		Bruteforce Approach
 		coins -> is holding the list of coins (only input)
 		sum -> is for target sum to produce by using the coin change	
 	*/
 	int minCoins(vector<int> coins, int sum) {
-
-
-
-		return 0;
+		// you already reached to a target sum
+		if (sum == 0) {
+			return 0;
+		}
+		int min_coin_count = INT_MAX;
+		for (int i = 0; i < coins.size(); ++i) {
+			if (coins[i] <= sum) {
+				min_coin_count = min(min_coin_count, 1 + minCoins(coins, sum- coins[i]));
+			}
+		}
+		
+		return min_coin_count;
 	}
 };
 
 int main() {
 	vector<int> coins = { 1,2,3,4,5 };
-	int sum = 12;
+	int sum = 24;
 	CoinChange cc;
 	cout << cc.minCoins(coins, sum);
 }
