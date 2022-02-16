2f29f458563e761eb41a4afd0833fecd0945a46c
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-02-16 18:45:13 +0530

    Reworking on it

--- 15-02-2022/Longest_Common_Subsequence.cpp
+++ 15-02-2022/Longest_Common_Subsequence.cpp
@@ -1,105 +1,79 @@
 /*
 	Longest Common Subsequence
+    Problem statement
+        Given two sequences, find the length of the longest subsequence present in both of them.
+        A subsequence is a sequence that appears in the same relative order but is not necessarily contiguous. 
+        For example, abc, abg, bdf, aeg, acefg…etc., are subsequences of abcdefg. 
+        So a string of length n has {2^{n}} different possible subsequences.
+    
+        Let us consider an example to understand this problem better.
+        1. LCS for input Sequences ABCDGH and AEDFHR is ADH of length 3.
+        2. LCS for input Sequences AGGTAB and GXTXAYB is GTAB of length 4.
 
 
 */
 
 //#define FORREF
 //#define APP01
+#define APP02
+#define APP03
 
 #ifndef FORREF
 using namespace std;
 #include<iostream>
 #include<vector>
+#include<stack>
 
 
-typedef vector<vector<int>> SubSeq;
+typedef stack<char> Sequence;
 typedef vector<vector<int>> Memo;
 
 struct LongestSubsequenceSeries {
 
-    int lcs_rec(char s1[], const int &s1_len, char s2[], const int &s2_len, int s1_ind, int s2_ind, SubSeq&memo) {
-        if (s1_ind >= s1_len || s2_ind >= s2_len) {
+    /*
+        There are two problem here
+        1. Find the longest common subsequence - Completed
+        2. Find the longest common subsequence string - TBD
+    */
+    int lcs_rec(char s1[], char s2[], int s1Len, int s2Len) {
+        if (s1Len == 0 || s2Len == 0) {
             return 0;
         }
-
+        
         int lcs1 = 0, lcs2 = 0, lcs3 = 0;
 
-        if (s1[s1_ind] == s2[s2_ind]) {
-            memo[s1_ind][s2_ind] = 1;
-            lcs1 = 1 + lcs_rec(s1, s1_len, s2, s2_len, s1_ind + 1, s2_ind + 1, memo);
+        if (s1[s1Len-1] == s2[s2Len-1]) {
+            lcs1 = 1 + lcs_rec(s1, s2, s1Len-1, s2Len-1);
         }
         else {
-            lcs2 = lcs_rec(s1, s1_len, s2, s2_len, s1_ind + 1, s2_ind, memo);
-            lcs3 = lcs_rec(s1, s1_len, s2, s2_len, s1_ind, s2_ind+1, memo);
+            lcs2 = lcs_rec(s1, s2, s1Len - 1, s2Len);
+            lcs3 = lcs_rec(s1, s2, s1Len, s2Len-1);
         }
         
         return max(lcs1, max(lcs2, lcs3));
     }
 
-    /*
-        As we observed here some the overlapping of sub structure is possible and that may be optimized with recursion and memoization.
-    */
-        int lcs_memo(char s1[], const int& s1_len, char s2[], const int& s2_len, int s1_ind, int s2_ind, SubSeq& subseq, Memo &memo) {
-        if (s1_ind >= s1_len || s2_ind >= s2_len) {
-            return 0;
-        }
-
-        if (memo[s1_ind][s2_ind] != 0) {
-            cout << "Hit the ball !!!" << endl;
-            return memo[s1_ind][s2_ind];
-        }
 
 
-        int lcs1 = 0, lcs2 = 0, lcs3 = 0;
-
-        if (s1[s1_ind] == s2[s2_ind]) {
-            subseq[s1_ind][s2_ind] = 1;
-            lcs1 = 1 + lcs_memo(s1, s1_len, s2, s2_len, s1_ind + 1, s2_ind + 1, subseq, memo);
-        }
-        else {
-            lcs2 = lcs_memo(s1, s1_len, s2, s2_len, s1_ind + 1, s2_ind, subseq, memo);
-            lcs3 = lcs_memo(s1, s1_len, s2, s2_len, s1_ind, s2_ind + 1, subseq, memo);
-        }
-
-        return memo[s1_ind][s2_ind] = max(lcs1, max(lcs2, lcs3));
-    }
 };
 
 // Driver code
 int main() {
     // AGGTAB , GXTXAYB 
-    char s1[] = { 'A','G','G','T','A','B' }, s2[] = { 'G','X','T','X','A','Y', 'B'};
-    int m = 6, n = 7;
+    //char s1[] = { 'A','G','G','T','A','B' }, s2[] = { 'G','X','T','X','A','Y', 'B'};
+    char s1[] = { 'A','B','C','D','G','H' }, s2[] = { 'A','E','D','F','H','R' };
+    int m = 6, n = 6;
    int LCS[7][7] = { 0 };
 
    // calling code below
     LongestSubsequenceSeries lss;
-    SubSeq subseq(m + 1, vector<int>(n + 1, 0));
-    Memo memo(m + 1, vector<int>(n + 1, 0));
+    Sequence seq;
 
 #ifndef APP01
-    cout << lss.lcs_rec(s1, m, s2, n, 0, 0, subseq);
+    cout << lss.lcs_rec(s1, s2, m, n);
     cout << endl;
-    for (int i = 0; i <= m; ++i) {
-        for (int j = 0; j <= n; ++j) {
-            if (subseq[i][j]) {
-                cout << s1[i] << "";
-            }
-        }
-    }
-    cout << endl;
-#endif // APP01
 
-    cout << lss.lcs_memo(s1, m, s2, n, 0, 0, subseq, memo) << endl;
-    for (int i = 0; i <= m; ++i) {
-        for (int j = 0; j <= n; ++j) {
-            if (subseq[i][j]) {
-                cout << s1[i] << "";
-            }
-        }
-    }
-    cout << endl;
+#endif // APP01
 
 
 
