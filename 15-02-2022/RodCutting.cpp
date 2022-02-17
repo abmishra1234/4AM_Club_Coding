4a6ec4cce72a270e2c5512ffbb43d29b59daa87d
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-02-17 18:17:35 +0530

    Updated the problem solution with Tabulation approach

--- 15-02-2022/RodCutting.cpp
+++ 15-02-2022/RodCutting.cpp
@@ -12,7 +12,7 @@
 	- Tabulization :  : Not started Yet
 */
 
-//#define FORREF
+#define FORREF
 #ifndef FORREF
 using namespace std;
 #include <iostream>
@@ -41,6 +41,26 @@ struct CuttingRod {
 		}
 		return memo[n];
 	}
+
+	// implement the code with tabulization of dp
+	int cutRoad_dp(int price[], int n) {
+		Memo dp(n + 1, -1);
+		
+		// rod length is 0 means no rod
+		dp[0] = price[0];
+		//rod length from l=1 to l=n
+		for (int i = 1; i <= n; ++i) {
+			int best_price = INT_MIN;
+			// cut possible on road
+			for (int j = 1; j <= i; ++j) {
+				best_price = max(best_price, price[j] + dp[i - j]);
+			}
+			dp[i] = best_price;
+		}
+
+		return dp[n];
+	}
+
 };
 
 int main() {
@@ -55,6 +75,10 @@ int main() {
 	Memo memo(n + 1, -1);
 	int answer = cr.cutRod(price, n, memo);
 	cout << "Your maximum revanue from length n = " << n << " , after cutting is : " << answer << endl;
+
+	answer = cr.cutRoad_dp(price, n);
+	cout << "dp with Tab ---> Your maximum revanue from length n = " << n << " , after cutting is : " << answer << endl;
+
 }
 
 
