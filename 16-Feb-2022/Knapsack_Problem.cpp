97bd5f133bc810c8d8d923ebe3ca5657e87adf37
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-02-19 09:14:35 +0530

    So for this implementation, Knapsack with all 03 approach completed. Check the code.

--- 16-Feb-2022/Knapsack_Problem.cpp
+++ 16-Feb-2022/Knapsack_Problem.cpp
@@ -1,55 +1,103 @@
 /*
-	Status : TBD
-
+	Status : Completed
 */
 
-//#define FORREF
+#define FORREF
 #ifndef FORREF
 using namespace std;
 #include<iostream>
 #include<vector>
 
+// This is for handling the Memoization table
+typedef vector<vector<int>> Memo;
+
 struct Knapsack {
-	
-	int solve(int W, int n, int wt[], int val[], int cum_wt, int cum_item, int ind) {
-		if (cum_wt > W) {
+	/*
+		This is the simplest way to do the knapsack problem
+		Recursion without memoization
+	*/
+	int solve(int wt[], int val[], int W, int ind/* start from last index*/) {
+
+		if (W <= 0) {
 			return 0;
 		}
 
-		if (cum_item > n) {
+		if (ind < 0) {
 			return 0;
 		}
 
-		if (ind >= n) {
-			return 0;
+		int maxv = INT_MIN, u = INT_MIN, v = INT_MIN;
+		if (wt[ind] <= W) {
+			u = val[ind] + solve(wt, val, W-wt[ind], ind - 1);
 		}
-
-		int maxv = INT_MIN, v = INT_MIN, w = INT_MIN;
-		if (wt[ind] <= W - cum_wt) {
-			v = val[ind] + solve(W, n, wt, val, cum_wt + wt[ind], cum_item + 1, ind + 1);
-		}
-		
-		w = solve(W, n, wt, val, cum_wt, cum_item, ind + 1);
-
-		maxv = max(v, w);
+		v = solve(wt, val, W, ind - 1);
+		maxv = max(u, v);
 		return maxv;
 	}
 
+	/*
+	This is the simplest way to do the knapsack problem
+	Recursion with memoization
+*/
+	int solve_memo(int wt[], int val[], int W, int ind/* start from last index*/, Memo &memo) {
+		if (W <= 0) {
+			return 0;
+		}
 
+		if (ind < 0) {
+			return 0;
+		}
 
+		if (memo[W][ind] != -1) {
+			cout << "hit the memo, wow!!!" << endl;
+			return memo[W][ind];
+		}
 
+		int u = INT_MIN, v = INT_MIN;
+		if (wt[ind] <= W) {
+			memo[W][ind] = max(memo[W][ind], val[ind] + solve_memo(wt, val, W - wt[ind], ind - 1, memo));
+		}
+		memo[W][ind] = max(memo[W][ind], solve_memo(wt, val, W, ind - 1, memo));
 
+		return memo[W][ind];
+	}
 
+	/*
+		DP based on tabulation
+	*/
+	int solve_dp(int wt[], int val[], int W/* knapsack capacity */, int nItems) {
+		Memo dp(nItems+1, vector<int>(W+1, 0));
+		
+		for (int i = 0; i <= nItems; ++i) {
+			for (int j = 0; j <= W; ++j) {
+				// this is the case when there is no item to pick or no capacity left inot knapsack?
+				if (i == 0 || j == 0) {
+					dp[i][j] = 0;
+				}
+				else if (wt[i-1] <= j) {
+					dp[i][j] = max(val[i-1] + dp[i - 1][j-wt[i-1]], dp[i - 1][j]);
+				}
+				else if(wt[i] > j)
+					dp[i][j] = dp[i - 1][j];
+			}
+		}
+
+		return dp[nItems][W];
+	}
 };
 
 int main()
 {
-	int val[] = { 60, 100, 120 };
-	int wt[] = { 10, 20, 30 };
-	int W = 50;
+	int val[] = { 60, 100, 120, 456 };
+	int wt[] = { 10, 20, 30, 50 };
+	int W = 80;
 	int n = sizeof(val) / sizeof(val[0]);
 	Knapsack kp;
-	cout << kp.solve(W, n,wt, val, 0,0,0);
+	cout << kp.solve(wt, val, W, n-1) << endl;
+	// next is how to implement memo in knapsack
+	Memo memo(W + 1, vector<int>(n, -1));
+	cout << "Memo : " << kp.solve_memo(wt, val, W, n - 1, memo) << endl;
+	cout << "Tabulation : " << kp.solve_dp(wt, val, W, n);
 	return 0;
 }
 
