d69bec3b04cd4a47ab606e7b3e8e8e037930a57c
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-02-20 09:10:08 +0530

    So, finally we are able to solve the recursion with memorization solution.
    Next action is for Tabulation Approach

--- 17-Feb-2022/LIS.cpp
+++ 17-Feb-2022/LIS.cpp
@@ -47,10 +47,43 @@ struct LIS {
 		}
 		else
 		{
-			maxlen = max(maxlen, solve_rec(arr, n - 1));
+			maxlen = 1; 
 		}
+
+		// This is the case when you are ignoring the current element
+		maxlen = max(maxlen, max(1, solve_rec(arr, n - 1)));
+
 		return maxlen;
 	}
+
+		int solve_memo(int arr[], int n, Memo &memo) {
+		if (n == 0) {
+			return memo[n] = 0;
+		}
+		else  if (n == 1) {
+			return memo[n] = 1;
+		}
+		else {
+			if (memo[n] != INT_MIN) {
+				cout << "hiting the memo!!!" << endl;
+				return memo[n];
+			}
+
+			if (arr[n - 2] < arr[n - 1]) {
+				memo[n] = max(1, 1 + solve_memo(arr, n - 1, memo));
+			}
+			else
+			{
+				memo[n] = 1;
+			}
+
+			// this is for ignoring the current and movng from next
+			memo[n] = max(memo[n], solve_memo(arr, n - 1, memo));
+
+		}
+		return memo[n];
+	}
+
 };
 
 
@@ -63,9 +96,13 @@ int main() {
 	int arr[] = { 10,9,3,5,4,11,7,8 };
 	int n = 8;
 
+	//int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+	//int n = sizeof(arr) / sizeof(arr[0]);
+
 	LIS lis;
 	cout << lis.solve_rec(arr, n) << endl;
-	
+	Memo memo(n + 1, INT_MIN);
+	cout << "Memo = " << lis.solve_memo(arr, n, memo) << endl;
 
 
 	return 0;
